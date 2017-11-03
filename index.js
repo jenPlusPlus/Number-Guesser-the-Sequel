ceb505d1b0971b38eba6279ddde216432fd621ad
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-03 11:13:14 -0600

    Compare numbers and display results on page

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ function getRandomIntInclusive(min, max) {
 
 var randomNumber = getRandomIntInclusive(1, 100);
 console.log('randomNumber: ', randomNumber);
+
+const guessInputField = document.getElementById('guess');
+const submitButton = document.getElementById('submit');
+
+const display = () => {
+  const body = document.querySelector('body');
+  const newDiv = document.createElement('div');
+  const compareResults = document.createTextNode(compareNumbers());
+  newDiv.appendChild(compareResults);
+  body.appendChild(newDiv);
+
+}
+
+const compareNumbers = () => {
+  if (guessInputField.value < randomNumber) {
+    return guessInputField.value + ' is too low!';
+  }
+  else if (guessInputField.value > randomNumber) {
+    return guessInputField.value + ' is too high!';
+  } else {
+    return guessInputField.value + ' is correct!';
+  }
+}
+
+const getGuessInput = (event) => {
+  event.preventDefault();
+  display();
+}
+
+
+
+submitButton.addEventListener('click', getGuessInput);
